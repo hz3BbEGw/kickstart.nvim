ed68415a11236243d8286a54d979d30c50aa652b
hz3BbEGw <olekmasojc1@gmail.com>
2025-04-18 18:23:14 +0200

    added snacks-explorer

--- lua/kickstart/plugins/snacks.lua
+++ lua/kickstart/plugins/snacks.lua
@@ -1,35 +1,47 @@
 return {
-	{
-		"folke/snacks.nvim",
-		priority = 1000,
-		lazy = false,
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
 
-		---@type snacks.Config
-		opts = {
-			bigfile = { enabled = true },
-			image = { enabled = true },
-			notify = { enabled = true },
-			indent = { 
-				enabled = true,
-				animate = { enabled = false }
-			},
-			notifier = {
-				enabled = true,
-				timeout = 2500,
-			},
-			animate = { enabled = false },
-			styles = {
-				notification = {
-					wo = { wrap = true } -- Wrap notifications
-				}
-			}
-		},
+    ---@type snacks.Config
+    opts = {
+      bigfile = { enabled = true },
+      image = { enabled = true },
+      notify = { enabled = true },
+      indent = {
+        enabled = true,
+        animate = { enabled = false },
+      },
+      notifier = {
+        enabled = true,
+        timeout = 2500,
+      },
+      animate = { enabled = false },
+      explorer = {
+        enabled = true,
+        replace_netrw = true,
+      },
+	  picker = {
+		enabled = true,
+        sources = {
+          explorer = {
+			layout = { preview = "main", preset = "sidebar" },
+          },
+        },
+	  },
+      styles = {
+        notification = {
+          wo = { wrap = true }, -- Wrap notifications
+        },
+      },
+    },
 
-		keys = {
-			-- Other
-			{ "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
-			{ "<leader>g", function() Snacks.lazygit() end, desc = "Lazy[g]it" },
-			{ "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-		},
-	}
+    keys = {
+	  { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+      { '<leader>n', function() Snacks.notifier.show_history() end, desc = 'Notification History' },
+      { '<leader>g', function()  Snacks.lazygit() end, desc = 'Lazy[g]it' },
+      { '<leader>un', function()  Snacks.notifier.hide() end, desc = 'Dismiss All Notifications' },
+    },
+  },
 }
